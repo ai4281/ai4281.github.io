7948ddf4681ea7cb43ba9b79486eb6af0dd0ea5f
ai4281 <ai4281@gmail.com>
2015-01-28 12:56:34 -0800

    audio test

---
+++ audioPlayback_test/audio.js
@@ -0,0 +1,22 @@
+function playSound(argSound)
+{    
+    var filepath='wrong.mp3'; //example
+
+    if (argSound == 'wrong')
+    {
+    	console.log("wrong");
+    	filepath='wrong.mp3'
+    }
+
+    if (argSound == 'right')
+    {
+    	console.log("right");
+    	filepath='right.mp3'
+    }
+
+    var audio = new Audio();   
+    audio.src = filepath;
+    audio.controls = true;
+    audio.autoplay = true;
+
+}
