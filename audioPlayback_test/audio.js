08fda9af1e749b9a6406b4855eac4a71de391bce
ai4281 <ai4281@gmail.com>
2015-01-28 13:12:47 -0800

    audio fix

--- audioPlayback_test/audio.js
+++ audioPlayback_test/audio.js
@@ -2,21 +2,20 @@ function playSound(argSound)
 {    
     var filepath='wrong.mp3'; //example
 
-    if (argSound == 'wrong')
-    {
-    	console.log("wrong");
-    	filepath='wrong_dis.mp3'
-    }
-
     if (argSound == 'right')
     {
     	console.log("right");
-    	filepath='right.mp3'
+        filepath='right.mp3';
+    }
+
+    if (argSound == 'wrong')
+    {
+    	console.log("wrong");
+        filepath='wrong_dis.mp3';
     }
 
     var audio = new Audio();   
     audio.src = filepath;
     audio.controls = true;
     audio.autoplay = true;
-
 }
